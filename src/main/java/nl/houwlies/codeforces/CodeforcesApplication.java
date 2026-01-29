7df958ed6153d4f58a62b283aaec52363cc35f53
Houwlies <Houwlies@Houwlies.Houwlies>
2026-01-29 18:04:39 +0100

    day1problem2: 3 tries

--- src/main/java/nl/houwlies/codeforces/CodeforcesApplication.java
+++ src/main/java/nl/houwlies/codeforces/CodeforcesApplication.java
@@ -1,26 +1,14 @@
 package nl.houwlies.codeforces;
 
-import nl.houwlies.codeforces.problems.HundredEasyProblems.Day1Problem1;
-
 import java.util.Scanner;
 
 public class CodeforcesApplication {
 
 	public static void main(String[] args) {
 
-    Day1Problem1 day1Problem1 = new Day1Problem1();
-    Scanner scan = new Scanner(System.in);
 
-    int one = scan.nextInt();
-    int[] array = new int[one];
 
-        for (int i = 0; i < one; i++) {
-        array[i] = scan.nextInt();
+
     }
-
-        day1Problem1.reverseArray(array);
-
-        scan.close();
-	}
 
 }
---
+++ src/main/java/nl/houwlies/codeforces/problems/HundredEasyProblems/Day1Problem2.java
@@ -0,0 +1,27 @@
+package nl.houwlies.codeforces.problems.HundredEasyProblems;
+
+import java.util.Scanner;
+
+public class Day1Problem2 {
+
+//    Scanner scan = new Scanner(System.in);
+//
+//    int amountOfKids = scan.nextInt();
+//
+//    int[] array = new int[amountOfKids];
+//
+//
+//        for (int i = 0; i < amountOfKids; i++) {
+//        array[i] = scan.nextInt();
+//    }
+//
+//    int border1 = scan.nextInt();
+//    int border2 = scan.nextInt();
+//    int totalAmountOfCandy = 0;
+//        for (int i = border1; i <= border2; i++) {
+//        totalAmountOfCandy += array[i];
+//    }
+//        System.out.println(totalAmountOfCandy);
+
+
+}
