3b6dee22a5e9797a903cba3db4aa2654d1aa439f
Houwlies <250162647+Houwlies@users.noreply.github.com>
2026-02-01 12:14:04 +0100

    day1problem4: whoops

--- src/main/java/nl/houwlies/codeforces/problems/HundredEasyProblems/Day1Problem4.java
+++ src/main/java/nl/houwlies/codeforces/problems/HundredEasyProblems/Day1Problem4.java
@@ -3,6 +3,51 @@ package nl.houwlies.codeforces.problems.HundredEasyProblems;
 public class Day1Problem4 {
 
 
-
+//    Scanner scan = new Scanner(System.in);
+//
+//    String[] arr = new String[2];
+//    String a = scan.next().toLowerCase();
+//    String b = scan.next().toLowerCase();
+//
+//    arr[0] = a;
+//    arr[1] = b;
+//
+//    int result = 0;
+//        if (isValidInput(arr)) {
+//        if (a.length() > b.length()) {
+//            System.out.println("1");
+//        }
+//
+//        if (a.length() < b.length()) {
+//            System.out.println("-1");
+//        }
+//
+//        if (a.length() == b.length()) {
+//            int length = a.length();
+//
+//            for (int i = 0; i < length; i++) {
+//                char aChar = a.charAt(i);
+//                char bChar = b.charAt(i);
+//                if (aChar > bChar) {
+//                    result = 1;
+//                    break;
+//                } else if (aChar < bChar) {
+//                    result = -1;
+//                    break;
+//                }
+//            }
+//        }
+//    }
+//        System.out.println(result);
+//        scan.close();
+//
+//    static Boolean isValidInput(String[] input) {
+//        for (String s : input) {
+//            if (s.isEmpty() || s.length() > 100) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 
 }
